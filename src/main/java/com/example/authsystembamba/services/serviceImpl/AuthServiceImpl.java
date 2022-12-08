54cdb1ff27bcb76fc69bbf269d6d28c090bb6a35
Hopeuche360 <hopechijuka@gmail.com>
2022-12-08 16:20:46 +0100

    added password encoder

--- src/main/java/com/example/authsystembamba/services/serviceImpl/AuthServiceImpl.java
+++ src/main/java/com/example/authsystembamba/services/serviceImpl/AuthServiceImpl.java
@@ -1,14 +1,13 @@
 package com.example.authsystembamba.services.serviceImpl;
 
-import com.example.authsystembamba.enums.Role;
 import com.example.authsystembamba.models.User;
 import com.example.authsystembamba.pojos.SignupRequest;
 import com.example.authsystembamba.repositories.UserRepository;
 import com.example.authsystembamba.services.AuthService;
-import com.example.authsystembamba.utils.Constants;
 import com.example.authsystembamba.utils.UserUtil;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import static com.example.authsystembamba.enums.Role.ROLE_USER;
@@ -21,12 +20,14 @@ public class AuthServiceImpl implements AuthService {
     private final UserUtil userUtil;
     private final ModelMapper mapper;
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public String signup(SignupRequest request) {
         userUtil.checkIfEmailAlreadyExist(request.getEmail());
         User user = mapper.map(request, User.class);
         user.setRole(ROLE_USER);
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
         userRepository.save(user);
         //TODO: SEND VERIFICATION EMAIL
         return REGISTRATION_SUCCESSFUL;
